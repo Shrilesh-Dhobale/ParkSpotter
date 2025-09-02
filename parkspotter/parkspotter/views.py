723b5728ecc896e1efe457ec08b4347f1bdd3b2f
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-02 16:57:16 +0530

    UserRegistration imported in views from models

--- parkspotter/parkspotter/views.py
+++ parkspotter/parkspotter/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.contrib import messages
+from .models import UserRegistration
 def index(request):
     data={
         'title': 'Dashboard'
