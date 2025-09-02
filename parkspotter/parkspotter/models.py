5e700f33d617f71ec771be10995c4aa96ef53b20
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-02 16:51:37 +0530

    UserRegistration class created

--- parkspotter/parkspotter/models.py
+++ parkspotter/parkspotter/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+class UserRegistration(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, unique=True)
+    password = models.CharField(max_length=128)
+    
+
+    
