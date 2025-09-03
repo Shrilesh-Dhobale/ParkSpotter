9df91bf004d83a5f951224e0dfd8d2ac6e174a97
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-03 16:53:02 +0530

    Separate app created for user

--- parkspotter/parkspotter/views.py
+++ parkspotter/parkspotter/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
-from django.contrib import messages
-from .models import UserRegistration
+from django.shortcuts import render
+
 def index(request):
     data={
         'title': 'Dashboard'
@@ -11,38 +10,8 @@ def index(request):
 def new_entry(request):
     return render(request, 'new_entry.html')
 
-def login(request):
-    return render(request, 'login.html')
-
 def recipt(request):
     return render(request, 'recipt.html')
-
-def user_registration(request):
-    if request.method == 'POST':
-        name = request.POST.get('full_name')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        password = request.POST.get('password')
-        confirm_password = request.POST.get('confirm_password')
-        if password != confirm_password:
-            messages.error(request, "Passwords do not match.")
-            return redirect('user_registration')
-        if UserRegistration.objects.filter(email=email).exists():
-            messages.error(request, "Email is already registered.")
-            return redirect('user_registration')
-        if UserRegistration.objects.filter(phone=phone).exists():
-            messages.error(request, "Phone number is already registered.")
-            return redirect('user_registration')
-
-        UserRegistration.objects.create(
-            full_name=full_name,
-            email=email,
-            phone=phone,
-            password=password
-        )
-        messages.success(request, "Registration successful.")
-        return redirect('login')
-    return render(request, 'user_registration.html')    
 
 def earning_report(request):
     return render(request, 'earning_report.html')
