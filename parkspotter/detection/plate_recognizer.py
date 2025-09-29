0dbab3cb400823e7b112d75525b1aab93d1cf718
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-09-29 17:09:31 +0530

    Cond added if webcam not open

--- parkspotter/detection/plate_recognizer.py
+++ parkspotter/detection/plate_recognizer.py
@@ -10,3 +10,6 @@ except FileNotFoundError:
 
 def recognize_and_store_plate():
     cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+        return
